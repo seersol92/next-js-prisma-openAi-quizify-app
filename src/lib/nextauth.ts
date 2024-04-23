df96354d0a8fb4b2b66635987db575392e402217
hAdi <seersol92@users.noreply.github.com>
2024-04-23 21:20:17 +0500

    setting up next auth

--- src/lib/nextauth.ts
+++ src/lib/nextauth.ts
@@ -1,6 +1,21 @@
-import { NextAuthOptions } from "next-auth"
+import { DefaultSession, NextAuthOptions } from "next-auth"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import {prisma} from '@/lib/db';
+import GoogleProvider from 'next-auth/providers/google'
+
+declare module 'next-auth/jwt' {
+    interface JWT {
+        id: string
+    }
+}
+
+declare module 'next-auth' {
+    interface Session extends DefaultSession {
+        user: {
+            id: string
+        } & DefaultSession['user']
+    }
+}
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
@@ -9,7 +24,34 @@ export const authOptions: NextAuthOptions = {
     maxAge: 7 * 24 * 60 * 60, // 7 days
   },
   secret: process.env.NEXTAUTH_SECRET,
-  providers: [],
   adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID as string,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+    })
+  ],
+  callbacks: {
+    jwt: async({token}) => {
+        const db_user = await prisma.user.findFirst({
+            where: {
+                email: token?.email
+            }
+        })
+        if(db_user)
+            token.id = db_user.id
+        return token;
+    },
+    session: async({session, token}) => {
+        if (token){
+            session.user.id = token.id
+            session.user.name = token.name
+            session.user.email = token.email
+            session.user.image = token.picture
+        }
+        return session;
+    },
+
+  },  
 }
 
